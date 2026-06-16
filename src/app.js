052c4d43b0f3dd679e77dd917f282b55ff2975ac
Anjaliicode <anjalisaini2224@gmail.com>
2026-06-16 17:42:43 +0530

    "checked http request"

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
-app.use("/test", (req, res) => {
-    res.send("hello i am a test you have succefully created a server")
+app.get("/user", (req, res) => {
+    res.send({ firstname: "Anjali", lastname: "saini" })
+})
+app.post("/user", (req, res) => {
+    res.send("i am succefully post the data to database")
+})
+app.delete("/user", (req, res) => {
+    res.send("i am succefully delete the data to database")
 })
 app.use("/server", (req, res) => {
     res.send("hello i am a new server you have succefully created a server")
